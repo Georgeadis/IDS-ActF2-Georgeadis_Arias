9794f2643b116f56a0246c57cb7ea2180308fd39
Georgeadis <79583086+Georgeadis@users.noreply.github.com>
2026-01-31 17:11:25 -0600

    Delete src directory

--- src/aplicacionsimple/Principal.java
+++
@@ -1,7 +0,0 @@
-package aplicacionsimple;
-
-public class Principal {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> new VentanaPrincipal().setVisible(true));
-    }
-}
